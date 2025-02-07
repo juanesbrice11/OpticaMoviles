1df2b55b35393eab7dda46fbc221a1240addc471
juanesbrice11 <juanesbrice977@gmail.com>
2025-02-07 13:25:11 -0500

    Creando base del proyecto

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const login = () => {
+  return (
+    <View>
+      <Text className='color-white'>Pagina de login</Text>
+    </View>
+  )
+}
+
+export default login
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,12 +3,31 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
+import {Link} from 'expo-router'
+
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Optica</Text>
+
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+
+
+      
+
+      <Link href="/profile">
+        <Text>profile</Text>
+      </Link>
+
+      <Link href="/login">
+        <Text>login</Text>
+      </Link>
+
+      <Link href="/register">   
+        <Text>register</Text>
+      </Link>
+
     </View>
   );
 }
