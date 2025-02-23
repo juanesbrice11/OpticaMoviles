49111d56e165865249f81675ff67289e55bac701
juanesbrice11 <juanesbrice977@gmail.com>
2025-02-22 20:35:06 -0500

    Äñadiendo pantalla de carga inicial y estableciendo pantalla principal login

--- app/index.tsx
+++ app/index.tsx
@@ -1,46 +1,28 @@
-import { StyleSheet } from "react-native";
-import { Text, View } from "@/components/Themed";
-import { Link } from "expo-router";
+import { useEffect, useState } from "react";
+import { useRouter, useRootNavigationState } from "expo-router";
+import { View, Text, ActivityIndicator } from "react-native";
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Bienvenido a la Óptica</Text>
+  const router = useRouter();
+  const navigationState = useRootNavigationState();
+  const [isLoading, setIsLoading] = useState(true);
 
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+  useEffect(() => {
+    if (navigationState?.key) {
+      setTimeout(() => {
+        router.replace("/(auth)/login");
+        setIsLoading(false);
+      }, 1000);
+    }
+  }, [navigationState?.key]);
 
-      <Link href="/(auth)/login">
-        <Text style={styles.link}>Iniciar sesión</Text>
-      </Link>
+  if (isLoading) {
+    return (
+      <View className="flex-1 items-center justify-center bg-white">
+        <ActivityIndicator size="large" color="blue" className="mt-4" />
+      </View>
+    );
+  }
 
-      <Link href="/(client)/client">
-        <Text style={styles.link}>Clientes</Text>
-      </Link>
-      <Link href="/(client)/crear">
-        <Text style={styles.link}>Crear Clientes</Text>
-      </Link>
-    </View>
-  );
+  return null;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-  link: {
-    fontSize: 16,
-    color: "blue",
-    textDecorationLine: "underline",
-  },
-});
