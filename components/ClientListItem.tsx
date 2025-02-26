807d9c56dcaefff1a2611117ec0baa6d5c0b3634
slesmes <santiagolesmesmarin@hotmail.com>
2025-02-26 01:15:46 -0500

    poniendo nuevos tokens en la parte del cliente

--- components/ClientListItem.tsx
+++ components/ClientListItem.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { View, Text } from "react-native";
 import { ClientSchema } from "@/types/api";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { textblack, textgray500 } from "./tokens";
+import { columleft, columright } from "./tokens";
 
 
 
@@ -10,32 +10,32 @@ export default function ClientListItem({ client }: { client: ClientSchema }) {
   return (
     <SafeAreaView className="p-4 border-gray-300 bg-white shadow-md shadow-gray-200 mb-3">
       <View className="flex-row justify-between">
-        <Text className="text-base font-semibold text-gray-700">Cédula:</Text>
-        <Text className="text-base text-gray-900">{client.cedula}</Text>
+        <Text className={columleft}>Cédula:</Text>
+        <Text className={columright}>{client.cedula}</Text>
       </View>
       <View className="border-b border-gray-200 my-2" />
 
       <View className="flex-row justify-between">
-        <Text className="text-base font-semibold text-gray-700">Nombre:</Text>
-        <Text className="text-base text-gray-900">{client.nombre}</Text>
+        <Text className={columleft}>Nombre:</Text>
+        <Text className={columright}>{client.nombre}</Text>
       </View>
       <View className="border-b border-gray-200 my-2" />
 
       <View className="flex-row justify-between">
-        <Text className="text-base font-semibold text-gray-700">Apellido:</Text>
-        <Text className="text-base text-gray-900">{client.apellido}</Text>
+        <Text className={columleft}>Apellido:</Text>
+        <Text className={columright}>{client.apellido}</Text>
       </View>
       <View className="border-b border-gray-200 my-2" />
 
       <View className="flex-row justify-between">
-        <Text className="text-base font-semibold text-gray-700">Teléfono:</Text>
-        <Text className="text-base text-gray-900">{client.telefono}</Text>
+        <Text className={columleft}>Teléfono:</Text>
+        <Text className={columright}>{client.telefono}</Text>
       </View>
       <View className="border-b border-gray-200 my-2" />
 
       <View className="flex-row justify-between">
-        <Text className="text-base font-semibold text-gray-700">Historia Clínica:</Text>
-        <Text className="text-base text-gray-900">{client.historiaClinica}</Text>
+        <Text className={columleft}>Historia Clínica:</Text>
+        <Text className={columright}>{client.historiaClinica}</Text>
       </View>
     </SafeAreaView>
   );
