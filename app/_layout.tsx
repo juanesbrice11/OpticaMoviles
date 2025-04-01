a347322369b7b5faa2dec3c18b8b63af56e516ab
juanesbrice11 <juanesbrice977@gmail.com>
2025-04-01 15:08:29 -0500

    Arreglando error de token undefined

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,27 +4,26 @@ import { AuthProvider } from "@/context/AuthContext";
 import { usePushNotifications } from "@/hooks/usePushNotifications";
 import { useEffect, useState } from "react";
 import NetInfo from "@react-native-community/netinfo";
-import { Text, View  } from "react-native";
+import { Text, View } from "react-native";
 
 export default function RootLayout() {
-
-  const { expoPushToken, notification } = usePushNotifications();
+  const { expoPushToken, notification } = usePushNotifications(); 
   const [isConnected, setIsConnected] = useState(false);
 
-
   useEffect(() => {
-      console.log("Push token", expoPushToken);
-      console.log("Notification", notification);
+    console.log("Push token", expoPushToken); 
+    console.log("Notification", notification); 
 
-      const unsubscribe = NetInfo.addEventListener(state => {
-          setIsConnected(state.isConnected as boolean);
-          console.log("NetInfo state", state);
-      });
+   
+    const unsubscribe = NetInfo.addEventListener(state => {
+      setIsConnected(state.isConnected as boolean);
+      console.log("NetInfo state", state); 
+    });
 
-      return () => {
-          unsubscribe();
-      }
-  }, []);
+    return () => {
+      unsubscribe();
+    };
+  }, [expoPushToken, notification]);
 
   return (
     <AuthProvider>
