5a39a9a92ce16b93b5be487d66865c1b1f0a4277
Camilo Andres Molano Aristizabal <camiangi1517@gmail.com>
2025-05-11 15:42:42 -0500

    Deactivate token support

--- services/notificationService.ts
+++ services/notificationService.ts
@@ -24,3 +24,27 @@ export const registerDeviceToken = async (deviceToken: DeviceToken, token: strin
         throw error;
     }
 };
+
+export const deactiveDeviceToken = async (deviceToken: DeviceToken, token: string) => {
+    try {
+        const response = await fetch(`${URL}/device-tokens/deactivate`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+            body: JSON.stringify(deviceToken),
+        });
+
+        if (!response.ok) {
+            const errorData = await response.text();
+            console.error('Server response:', errorData);
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        return response.json();
+    } catch (error) {
+        console.error('Error al desactivar token:', error);
+        throw error;
+    }
+};
